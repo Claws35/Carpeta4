2a87b587d113d4a44d5b27cda6c882b58a6d2eb3
Claws35 <mateocuti@gmail.com>
2025-05-15 15:54:02 -0300

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import formaCompleta from "./img/preuba2.png";
 import { WorkExperienceSection } from "./components/WorkExperience/WorkExperienceSection";
 import { ProjectsSection } from "./components/Projects/ProjectsSection";
+import imagenPofolio from "./img/yoImgportfolio.jpg";
 
 function App() {
   return (
@@ -41,6 +42,35 @@ function App() {
 
       <WorkExperienceSection />
       <ProjectsSection />
+      <div className="flex flex-col items-start max-w-[800px] gap-6 mx-auto h-[500px] mt-10">
+        <h2 className="font-[new-order] text-[52px] font-bold leading-none  mb-5">
+          Sobre mí
+        </h2>
+        <div className="flex   ">
+          <div className="max-w-[500px]">
+            <p>
+              Me llamo Mateo Cutinella, estudiante avanzado en la licenciatura
+              en diseño Multimedia y desde siempre he tenido una pasión por el
+              diseño y la creatividad. Mi viaje comenzó de pequeño, cuando
+              pasaba horas experimentando con programas de dibujo digital y
+              explorando cómo combinar colores e ideas para dar vida a historias
+              visuales. <br /> <br /> Aunque en ese entonces era solo un hobby,
+              pronto me di cuenta de que quería dedicarme a algo que me
+              permitiera unir mi creatividad con la tecnología Cuando no estoy
+              diseñando, me gusta investigar sobre nuevas tecnologías,
+              descubriendo cómo las herramientas digitales pueden llevar mi
+              trabajo al siguiente nivel.
+            </p>
+          </div>
+          <div className="w-[300px] h-[300px] overflow-hidden">
+            <img
+              className="w-full h-full rounded-full object-cover"
+              src={imagenPofolio}
+              alt="Portfolio"
+            />
+          </div>{" "}
+        </div>
+      </div>
     </div>
   );
 }
